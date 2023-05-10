49a80bd2a757aaeb161950616de7d6970e6128bf
Boris Brönner <broenner@xelonic.com>
2023-05-10 13:06:28 +0200

    add server-side version of block

---
+++ src/edit.js
@@ -0,0 +1,44 @@
+import { __ } from '@wordpress/i18n';
+import { useBlockProps } from '@wordpress/block-editor';
+import { Placeholder, TextControl } from '@wordpress/components';
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './editor.scss';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
+ *
+ * @return {WPElement} Element to render.
+ */
+export default function Edit({ attributes, setAttributes }) {
+	return (
+    <div { ...useBlockProps() }>
+      <Placeholder
+        label={ __('Ticker', 'xelonic-financial-ratio-block') }
+        instructions={ __('Set your ticker', 'xelonic-financial-ratio-block') }
+      >
+        <TextControl
+          value={ attributes.ticker }
+          onChange={ (val) => setAttributes({ ticker: val }) }
+        />
+      </Placeholder>
+      <Placeholder
+        label={ __('Ratio ID', 'xelonic-financial-ratio-block') }
+        instructions={ __('Set the ratio ID', 'xelonic-financial-ratio-block') }
+      >
+        <TextControl
+          value={ attributes.ratio_id }
+          onChange={ (val) => setAttributes({ ratio_id: val }) }
+        />
+      </Placeholder>
+    </div>
+	);
+}
---
+++ src/render.php
@@ -0,0 +1,83 @@
+<!--
+export function render_widget(ticker, block_props, is_editor) {
+  let fetch_error = false;
+  let title = "";
+  let subtitle = "";
+  let value = null;
+  let unit = null;
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+    };
+
+    fetch(`http://localhost:8081/market-data/1.0.0/external/wordpress/financial-ratio-block?ticker=${ticker}&ratio_id=market_cap`, options)
+      .then((response) => response.json())
+      .then((response) => {
+        title = response.title;
+        subtitle = response.subtitle;
+        value = response.value;
+        unit = response.unit;
+      })
+      .catch((err) => {
+        fetch_error = true;
+        console.error(err);
+      });
+  }, []);
+
+  if (fetch_error) {
+    return (
+      <div { ...block_props }>
+        <div>{ ticker }</div>
+        { is_editor ? (<div>{ __('Failed to update data, please try again.', 'xelonic-financial-ratio-block') }</div>) : (<div><span class="dashicons dashicons-warning"></span></div>) }
+      </div>
+    );
+  }
+
+  return (
+    <div { ...block_props }>
+      <div>{ ticker }</div>
+      <div>{ __( title, 'xelonic-financial-ratio-block') }</div>
+      <div>{ __( subtitle, 'xelonic-financial-ratio-block') }</div>
+      { render_ratio_value(value, unit) }
+    </div>
+  );
+}
+
+function render_ratio_value(value, unit) {
+  if (!value) {
+    return (<div>n/a</div>);
+  }
+
+  let unit_output = "";
+  if (unit) {
+    unit_output = " " + unit.name;
+  }
+
+  return (
+    <div>{ value.value }{ unit_output }</div>
+  );
+}
+
+-->
+<?php
+	$ticker = $attributes['ticker'];
+	$ratio_id = $attributes['ratio_id'];
+
+	$error = "";
+	$response = wp_remote_get( "http://host.docker.internal:8081/market-data/1.0.0/external/wordpress/financial-ratio-block?ticker=$ticker&ratio_id=$ratio_id" );
+	if (is_wp_error($response)) {
+		$error = $response->get_error_message();
+	}
+
+	$body     = wp_remote_retrieve_body( $response );
+?>
+
+<div <?php echo get_block_wrapper_attributes(); ?>>
+	<!-- <?php esc_html_e( 'Financial Ratio Block – hello from a dynamic block!', 'financial-ratio-block' ); ?> -->
+
+	<div>ticker: <?= $ticker ?></div>
+	<div>ratio ID: <?= $ratio_id ?></div>
+	<div>error: <?= $error ?></div>
+	<div>response: <?= $body ?></div>
+</div>
