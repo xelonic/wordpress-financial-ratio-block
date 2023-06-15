a1b6e5ab49132c294e5b77882707115b4a0cdb03
Boris Brönner <broenner@xelonic.com>
2023-06-15 12:48:44 +0200

    add script to update the svn repo

---
+++ update-svn.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -euo pipefail
+set -x
+
+script_name="$(basename $0)"
+
+print_usage() {
+  echo "${script_name} <source git directory> <target svn directory>"
+}
+
+if (( $# < 2 ));
+then
+  print_usage
+  exit 1
+fi
+
+source_dir=$1; shift
+target_dir=$1; shift
+
+target_trunk="${target_dir}/trunk"
+
+if [[ -d "${target_trunk}" ]];
+then
+  rm -IR "${target_trunk}"
+fi
+
+find "${source_dir}" \
+  -type d \( -name "node_modules" -o -name "assets" -o -name ".git" -o -name ".github" \) -prune \
+  -o -type f \( -name ".gitignore" -o -name "${script_name}" \) -prune \
+  -o -type f -print0 |
+while IFS= read -r -d '' file; do
+    rel_path="${file#$source_dir}"
+    target_subdir="${target_trunk}${rel_path%/*}"
+
+    mkdir -p "${target_subdir}"
+    cp "${file}" "${target_subdir}"
+done
+
+cp -R "${source_dir}/assets"/* "${target_dir}/assets"
